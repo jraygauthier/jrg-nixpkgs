4bfbf4048249912893d4f311550a6fcda2259119
Raymond Gauthier <jraygauthier@gmail.com>
2025-07-25 15:51:33 -0400

    fix(flake): fix jrg-nixpkgs-hello script and meta
     -  No interpreter was specified as part of the resulting script. This
        lead to runtime errors. Fixed by using `writeShellScriptBin`.
     -  No meta was provided for this package. Remedy to that.

--- .nix/pkgs/jrg-nixpkgs-hello/default.nix
+++ .nix/pkgs/jrg-nixpkgs-hello/default.nix
@@ -1,4 +1,12 @@
-{ writeScriptBin }:
-writeScriptBin "jrg-nixpkgs-hello" ''
+{ lib, writeShellScriptBin }:
+(writeShellScriptBin "jrg-nixpkgs-hello" ''
   echo "Hello!"
-''
+'').overrideAttrs
+  (old: {
+    meta = with lib; {
+      description = "A hello script shipped by the jrg-nixpkgs flake";
+      homepage = "https://github.com/jraygauthier/jrg-nixpkgs";
+      license = licenses.mit;
+      maintainers = with maintainers; [ jraygauthier ];
+    };
+  })
